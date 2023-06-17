456b4ce886ca8bf7a202f7835ea3b0c2558c0624
Vlad <vladgerasimov94@yandex.ru>
2023-06-17 20:40:33 +0300

    lesson 5. Added test for people, used jsonpath

--- tests/test_people.py
+++ tests/test_people.py
@@ -3,7 +3,8 @@ from uuid import uuid4
 
 import requests
 from assertpy.assertpy import assert_that, soft_assertions
-import steps
+from jsonpath_ng import parse
+
 
 from config import BASE_URI
 
@@ -21,7 +22,7 @@ class TestPeople:
             assert_that(response_dict).extracting("fname").is_not_empty().contains('Kent')
 
     def test_new_person_can_be_added(self):
-        unique_last_name = self._create_new_person()
+        unique_last_name = self.create_person_with_unique_last_name()
 
         # After user is created, we read all the users and then use filter expression to find if the
         # created user is present in the response list
@@ -30,7 +31,7 @@ class TestPeople:
         assert_that(is_new_user_created).is_not_empty()
 
     def test_created_person_can_be_deleted(self):
-        persons_last_name = self._create_new_person()
+        persons_last_name = self.create_person_with_unique_last_name()
 
         peoples = requests.get(BASE_URI).json()
         newly_created_user = self._search_created_user_in(peoples, persons_last_name)[0]
@@ -42,14 +43,33 @@ class TestPeople:
         all_ids = [person["person_id"] for person in peoples_fresh]
         assert_that(all_ids).does_not_contain(newly_created_user["person_id"])
 
-    def _create_new_person(self):
-        # Ensure a user with a unique last name is created everytime the test runs
-        # Note: json.dumps() is used to convert python dict to json string
-        unique_last_name = f'User {str(uuid4())}'
-        payload = {
-            'fname': 'New',
-            'lname': unique_last_name
-        }
+    def test_person_can_be_added_with_a_json_template(self, create_data):
+        self.create_person_with_unique_last_name(create_data)
+
+        response = requests.get(BASE_URI)
+        peoples = json.loads(response.text)
+
+        # Get all last names for any object in the root array
+        # Here $ = root, [*] represents any element in the array
+        # Read full syntax: https://pypi.org/project/jsonpath-ng/
+        jsonpath_expr = parse("$.[*].lname")
+        result = [match.value for match in jsonpath_expr.find(peoples)]
+
+        expected_last_name = create_data['lname']
+        assert_that(result).contains(expected_last_name)
+
+    def create_person_with_unique_last_name(self, body=None):
+        if body is None:
+            # Ensure a user with a unique last name is created everytime the test runs
+            # Note: json.dumps() is used to convert python dict to json string
+            unique_last_name = f'User {str(uuid4())}'
+            payload = json.dumps({
+                'fname': 'New',
+                'lname': unique_last_name
+            })
+        else:
+            unique_last_name = body['lname']
+            payload = json.dumps(body)
 
         # Setting default headers to show that the client accepts json
         # And will send json in the headers
@@ -59,7 +79,7 @@ class TestPeople:
         }
 
         # We use requests.post method with keyword params to make the request more readable
-        response = requests.post(url=BASE_URI, json=payload, headers=headers)
+        response = requests.post(url=BASE_URI, data=payload, headers=headers)
         assert_that(response.status_code, description='Person not created').is_equal_to(requests.codes.no_content)
         return unique_last_name
 
